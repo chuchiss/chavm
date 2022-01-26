85e6da1ece432510adf5c2e6664c62be0f4189d2
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 02:50:30 -0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -11,6 +11,7 @@ let handler = async (m, { conn }) => {
   } finally {
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
     let { name, limit, exp, lastclaim, registered, warning, robos, like, banned, regTime, age, level } = global.DATABASE.data.users[who]
+    
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
@@ -20,7 +21,7 @@ Limit: ${limit}
 Mg: ${like}
 Advertencias: ${warning} / 5
 Robos: ${robos}
-Baneado: ${banned}
+Baneado: ${banned ? 'si' : 'no'}
 `.trim()
     let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
