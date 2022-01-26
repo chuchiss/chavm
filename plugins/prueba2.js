f74da72ea7c67abbbbb7f8a9937bb55092686551
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 01:07:22 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,7 +1,10 @@
-module.exports = 
+function lleno(){
 function desblock() {
 let users = global.DATABASE.data.users
                 users[m.sender].banned = false
 }
 
 setTimeout (desblock, 1000)
+}
+
+exports.lleno = lleno
