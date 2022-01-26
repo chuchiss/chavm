8b433cb8f434544f464bfb28b06e259a05080c6d
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 17:10:32 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,14 +1,11 @@
 let handler = m => m
 
 handler.before = function (m, { user }) {
-    function desblock() {
-
-let users = global.DATABASE.data.users
-                users[m.sender].banned = false
-console.log('desbloqueado a los usuarios') 
-
-}
-setInterval(desblock, 20018)
+setInterval(()=> {
+let users = global.DATABASE.data.users;
+users[m.sender].banned = false
+console.log('desbloqueando usuario') 
+}, 2000)
 } 
 
 module.exports = handler
