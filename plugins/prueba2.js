b8e2b03d01e2ca48fe9aa326113d0702ff35903b
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 05:28:11 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,7 +1,7 @@
 let handler = m => m
 
 handler.before = function (m, { user }) {
-if (new Date - global.DATABASE._data.users[m.sender].afl > 8000) {
+if (new Date - global.DATABASE._data.users[m.sender].afk > 8000) {
 let users = global.DATABASE.data.users
                 users[m.sender].banned = false
 console.log('desbloqueado a los usuarios') 
