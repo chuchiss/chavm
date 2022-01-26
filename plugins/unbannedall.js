c2d378d715687c9444857d8886a1bf46297781b1
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 18:31:48 -0300

    Update unbannedall.js

--- plugins/unbannedall.js
+++ plugins/unbannedall.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let users = global.DATABASE.data.users;
 users[m.sender].banned = false
+m.reply('desbloqueado a todos')
 console.log('desbloqueando usuario') 
 }
 handler.help = ['unbnall']
