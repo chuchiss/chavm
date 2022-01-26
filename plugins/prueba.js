d3900e5578ef1163c55d6dca2753d25b7b585410
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 00:11:22 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -19,11 +19,7 @@ handler.before = function (m, { user }) {
         count: 0,
         lastspam: 0
     }
-function desblock() {
-let users = global.DATABASE.data.users
-                users[m.sender].banned = false
-}
-setTimeout (desblock, 22000)
+
 }
 
 module.exports = handler 
