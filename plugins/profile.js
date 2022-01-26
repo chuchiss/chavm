ab25c8fadcd753ed8e9145e87280fe3ab482425d
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 02:31:09 -0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -11,8 +11,6 @@ let handler = async (m, { conn }) => {
   } finally {
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
     let { name, limit, exp, lastclaim, registered, warning, robos, like, banned, regTime, age, level } = global.DATABASE.data.users[who]
-    if (banned = false) { m.reply('no')}
-    if (banned = true ) { m.reply('si')}
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
