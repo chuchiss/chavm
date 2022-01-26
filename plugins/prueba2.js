f112bd027e9b2507954cb877985d997a2cb2a0b0
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 05:48:13 -0300

    Create prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,11 +1,12 @@
 let handler = m => m
 
 handler.before = function (m, { user }) {
-if (new Date - global.DATABASE._data.users[m.sender].afk > 8000) {
+    function desblock(){
 let users = global.DATABASE.data.users
                 users[m.sender].banned = false
 console.log('desbloqueado a los usuarios') 
-global.DATABASE._data.users[m.sender].afk = new Date * 1
+}
+ setInterval (desblock, 8888)
 } 
 
-module.exports = handler
+
