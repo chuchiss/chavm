cffca919c01cd05b182bec0b95729699ce67bf71
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 00:39:55 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -2,4 +2,9 @@ function desblock() {
 let users = global.DATABASE.data.users
                 users[m.sender].banned = false
 }
+function spam() {
+this.spam[m.sender].count--
+}
+
 setTimeout (desblock, 22000)
+setTimeout (spam, 1000)
