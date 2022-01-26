3810f6f884aa2f3f60e01d39e1b74292a6f7df6c
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 01:30:39 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -4,12 +4,14 @@ handler.before = function (m, { user }) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
-            if (this.spam[m.sender].count > 8) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > ) {
+            if (this.spam[m.sender].count > 5) {
                 let users = global.DATABASE.data.users
                 //users[m.sender].banned = true
                 m.reply('*No agas Spam!!*')
-                this.spam[m.sender].count--
+                this.spam[m.sender].count-- 
+                this.spam[m.sender].lastspam--
+
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
