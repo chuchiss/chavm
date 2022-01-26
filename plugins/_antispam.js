5e99e3b2a73be224b27bebda7170c62673dd49e2
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 20:33:27 -0300

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -4,8 +4,8 @@ handler.before = function (m, { user }) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 8) {
-            if (this.spam[m.sender].count > 8) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+            if (this.spam[m.sender].count > 10) {
                 let users = global.DATABASE.data.users
                 users[m.sender].banned = true
                setTimeout(()=> {
