710a9ce1b673470f025dda0ece9a5fe89b0ced0a
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-25 23:24:14 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -4,7 +4,7 @@ handler.before = async function (m, { conn, user } ) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 1) {
             if (this.spam[m.sender].count > 8) {
                 global.DATABASE._data.users[m.sender].banned = true
                 m.reply('*No agas Spam!!*')
