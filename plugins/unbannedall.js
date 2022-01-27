9c6afefd2a9eb34b1ad9be829b00deaf489bcae5
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-27 18:22:41 -0300

    Update unbannedall.js

--- plugins/unbannedall.js
+++ plugins/unbannedall.js
@@ -1,11 +1,14 @@
-
-const fetch = require('node-fetch')
-
-let handler = async (m, { conn, user }) => {
-let users = global.DATABASE.data.users;
-users[m.sender].banned = false
-m.reply('desbloqueado a todos')
-console.log('desbloqueando usuario') 
+let { Presence } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args }) => {
+	await conn.updatePresence(m.chat, Presence.composing) 
+	let list = Object.entries(global.DATABASE.data.users)
+	if(!args || !args[0]) {
+		var lim = false
+	} else {
+		var lim = parseInt(args[0])
+	}
+	list.slice(0, list.length).map(([user, data], i) => (Number(data.banned = lim)))
+		conn.reply(m.chat, `*baneados restablecido con éxito ${lim} / user*`, m)
 }
 handler.help = ['unbanall']
 handler.tags = ['owner']
