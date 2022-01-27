55f0cac075ed907133327ac27b4b6bc26548fad4
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 23:00:10 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -2,23 +2,21 @@ let { promisify } = require('util')
 let _gis = require('g-i-s')
 let gis = promisify(_gis)
 let Rimage = require('../lib/image')
-
 let handler  = async (m, { conn, args }) => {
-function Romage(){
-let text = `“${pickkRandom(global.bucina)}”`
+function Roimage() {
+  let text = `“${pickkRandom(global.bucina)}”`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
- if (!gimage) await Romage()
- if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
+  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
   conn.sendFile(m.chat, url, 'gimage', `
-MEME `.trim(), m)
-}
-catch (e){ await Romage() }
+MEME`.trim(), m)
+catch (e) {await Roimage()} } 
 }
 handler.help = ['meme', 'memes']
 handler.tags = ['internet', 'tools']
-handler.command = /^(meme|memes)$/i
+handler.command = /^(me(me)|memes)$/i
 handler.group = true
+handler.exp = 750
 
 module.exports = handler
 
@@ -26,23 +24,42 @@ function pickRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
-afunction pickkRandom(list) {
+function pickkRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 
 global.bucina = [
-  'meme gatitos',
-  'meme perros',
-  'memes del dia',
-  'memes',
-  'memes 2022',
-  'memes graciosos',
-  'meme gracioso',
-  'memes paises',
-  'memes de risa',
-  'memes español',
-  'memes de instagram',
-  'meme de instagram',
-  'memes de facebook',
-  'meme  de facebook',
+  'meme graciosos jpg',
+  'meme espanol 2019 jpg',
+  'meme graciosos de autos',
+  'meme espanol jpg',
+  'meme graciosos 2020',
+  'meme espanol 2018',
+  'meme gracioso de bicis',
+  'meme espanol 2021',
+  'meme graciosos de perros',
+  'meme espanol de gatos',
+  'meme graciosos arbol',
+  'meme enfermos gatos',
+  'meme graciosos',
+  'meme ultimo meme del dia',
+  'meme sobrenatural 2019',
+  'meme graciosos de autos',
+  'meme espanol ciudades',
+  'meme graciosos 2020',
+  'meme lindo 2021',
+  'meme de peliculas',
+  'meme boludos',
+  'meme gracioso de bicis',
+  'meme espanol 2021',
+  'meme graciosos de perros',
+  'meme instagram',
+  'meme osos arbol',
+  'meme argetina',
+  'meme colombia',
+  'meme brasil',
+  'meme afeminado',
+  'meme chile',
+  'meme venezuela',
+  'meme hd 4k',
 ]
