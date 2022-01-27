91e5fbc412c4bc408027f6de1ea1760a8276e48b
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-26 22:02:45 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -5,11 +5,9 @@ let Rimage = require('../lib/image')
 let handler  = async (m, { conn, args }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastmp > 2400) {
   let text = `“${pickkRandom(global.bucina)}”`
-  m.reply('${text}')
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
   if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (!gis)  console.error(e)
   conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
