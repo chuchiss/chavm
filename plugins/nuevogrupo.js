4b18a6beead9438451fba309de4af3def6ee4ceb
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-28 16:35:32 -0300

    Update nuevogrupo.js

--- plugins/nuevogrupo.js
+++ plugins/nuevogrupo.js
@@ -3,9 +3,9 @@ let handler  = async (m, { conn, text}) => {
   if (!text){
     conn.reply(m.chat,'Ingresa el link del grupo para añadirme :)\n ejemplo  .nuevogrupo http://whatsapp.com/group=?339493',m)
   }else {
-    conn.reply(global.owner + '@s.whatsapp.net',`*Grupo enviado de: @${m.sender.split('@')[0]}*\n\n${text}`, m,{contextInfo: {
-      mentionedJid: [m.sender]
-    }})
+ //   conn.reply(global.owner + '@s.whatsapp.net',`*Grupo enviado de: @${m.sender.split('@')[0]}*\n\n${text}`, m,{contextInfo: {
+    //  mentionedJid: [m.sender]
+   // }})
     conn.reply(m.chat,`Puedes agregarme a tu grupo\n\nSeguime en ig @insanebot.oficial en ig\n\nSabías el hecho importante de que los grupos tienen que tener al menos mas de 25 participantes`,m)
   }
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
