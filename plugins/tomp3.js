31e69c4e3dda336dcc9143485d1c6f8dcd1cbceb
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-30 04:10:04 -0300

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,20 +1,27 @@
-const { toPTT } = require('../lib/converter')
-const { MessageType } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan caption *${usedPrefix + command}*`
-  let media = await q.download()
-  let audio = await toPTT(media, 'mp3')
-  conn.sendMessage(m.chat, audio, MessageType.audio, {
-    quoted: m,
-    ptt: true
-  })
+let { webp2mp4 } = require('../lib/webp2mp4')
+let { ffmpeg } = require('../lib/converter')
+let handler = async (m, { conn }) => {
+    if (!m.quoted) throw 'Reply sticker or audio!'
+    let mime = m.quoted.mimetype || ''
+    if (!/webp|audio/.test(mime)) throw 'Reply sticker or audio!'
+    let media = await m.quoted.download()
+    let out = Buffer.alloc(0)
+    if (/webp/.test(mime)) {
+        out = await webp2mp4(media)
+    } else if (/audio/.test(mime)) {
+        out = await ffmpeg(media, [
+            '-filter_complex', 'color',
+            '-pix_fmt', 'yuv420p',
+            '-crf', '51',
+            '-c:a', 'copy',
+            '-shortest'
+        ], 'mp3', 'mp4')
+    }
+    await conn.sendFile(m.chat, out, 'out.mp4', null, m)
 }
 handler.help = ['iphone (tomp3) (reply)']
 handler.tags = ['audio']
 
-handler.command = /^iphone$/i
+handler.command = /^(iphone)$/i
 
 module.exports = handler
