1242ddb8486f4b00a30bdb636109512a25c135c4
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-30 04:13:20 -0300

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -6,17 +6,9 @@ let handler = async (m, { conn }) => {
     if (!/webp|audio/.test(mime)) throw 'Reply sticker or audio!'
     let media = await m.quoted.download()
     let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
+    if (/webp|audio/.test(mime)) {
         out = await webp2mp4(media)
-    } else if (/audio/.test(mime)) {
-        out = await ffmpeg(media, [
-            '-filter_complex', 'color',
-            '-pix_fmt', 'yuv420p',
-            '-crf', '51',
-            '-c:a', 'copy',
-            '-shortest'
-        ], 'mp3', 'mp4')
-    }
+    } 
     await conn.sendFile(m.chat, out, 'out.mp4', null, m)
 }
 handler.help = ['iphone (tomp3) (reply)']
