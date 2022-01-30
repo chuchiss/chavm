896ac2614028b8181c0b31571b9ad06b080a7fe1
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-30 04:00:39 -0300

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,17 +1,18 @@
-const { toAudio } = require('../lib/converter')
+const { toPTT } = require('../lib/converter')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) return m.reply('error')
+  if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan caption *${usedPrefix + command}*`
   let media = await q.download()
-  let audio = await toPTT(media, 'mp3')
+  let audio = await toPTT(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {
-    quoted: m, mimetype: 'audio/mp3'
+    quoted: m,
+    ptt: true
   })
 }
-handler.help = ['iphone tomp3 (reply)']
+handler.help = ['iphone (tomp3) (reply)']
 handler.tags = ['audio']
 
 handler.command = /^iphone$/i
