5a2b84c3a4981e99f6827c82eef1243b405d31e2
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-31 04:38:26 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,5 +1,6 @@
 global.tutti = global.tutti ? global.tutti : {}
-let handler = async (m, {conn}) => {
+let handler = async (m, { conn, users}) => {
+if (new Date - global.DATABASE._data.users[m.sender].lastmp > 10400) {
 let id = m.chat
 if (id in global.tutti) return conn.reply(m.chat, 'sjdjdjkd', global.tutti[id][0])
 
@@ -15,12 +16,14 @@ m.reply(' nombre , apellido , cosa , animal , pais ')
 } function final(){
 delete global.tutti[id]
 }
+
 global.tutti[id] = [
-    await setTimeout(awaiting, 4000)
+setTimeout(awaiting, 4000)
 setTimeout(awaitingg, 8000)
 setTimeout(tutti, 12000)
 setTimeout(final, 20000)
   ]
+} else return m.reply('todavia estas en juego')
 }
 
 handler.help = ['tutti']
