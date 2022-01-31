86cf9001a7834565d49c5db09de9ca5efb1f3bbf
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-31 03:59:53 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,12 +1,25 @@
+
 let handler = async (m, {conn}) => {
 m.reply('you are ready for the tutti frutti? \n Minimo 2 jugadores')
 function awaiting(){
- m.reply('esperando compañero..')
+ m.reply('Esperando compañero..')
+}
+function awaitingg(){
+m.reply('el que termina primero dice basta \n\nPreparado ...\n\n Listo???') }
+function tutti(){
+conn.reply(m.chat,` empieza la letra “${pickRandom(global.chicle)}”`, m)
+m.reply(' nombre , apellido , cosa , animal , pais ')
 }
 setTimeout(awaiting, 4000)
+setTimeout(awaitingg, 8000)
+setTimeout(tutti, 12000)
 }
 handler.help = ['tutti']
 handler.tags = ['']
 handler.command = /^(tutti)$/i
 
 module.exports = handler
+
+global.chicle = [
+'a','b','c','d','e','f','g','h','i','o','u'
+]
