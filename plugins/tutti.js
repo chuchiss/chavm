0f608bf50c4ec26396c826b44c37f8edeba36837
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-31 04:29:02 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,7 +1,7 @@
 global.tutti = global.tutti ? global.tutti : {}
 let handler = async (m, {conn}) => {
 let id = m.chat
-if (id in global.tutti) return m.reply(' Espera terminar el tutti frutti anterior ' )
+if (id in global.tutti) return conn.reply(m.chat, 'sjdjdjkd', global.tutti[id][0])
 
 m.reply('you are ready for the tutti frutti? \n Minimo 2 jugadores')
 function awaiting(){
@@ -12,12 +12,14 @@ m.reply('el que termina primero dice basta \n\nPreparado ...\n\n Listo???') }
 function tutti(){
 conn.reply(m.chat,` empieza la letra “${pickRandom(global.chicle)}”`, m)
 m.reply(' nombre , apellido , cosa , animal , pais ')
+} function final(){
 delete global.tutti[id]
 }
 global.tutti[id] = [
     await setTimeout(awaiting, 4000)
 setTimeout(awaitingg, 8000)
 setTimeout(tutti, 12000)
+setTimeout(final, 20000)
   ]
 }
 
