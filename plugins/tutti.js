32bce508f49e765137c964f3c5975cc8d0ed7e07
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-01-31 03:43:10 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,7 +1,7 @@
 let handler = async (m, {conn}) => {
 if (/tutti/.test(command)) return m.reply('you are ready for the tutti frutti? \n Minimo 2 jugadores\nescribe .jugar')
 if (/listo/.test(command)) return m.reply('esperando compañero..')
-
+}
 handler.help = ['tutti']
 handler.tags = ['']
 handler.command = /^(tutti)$/i
