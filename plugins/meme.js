8b7864f698c2ca064541c1127b6e30d0f3412ef7
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-04 11:46:10 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -9,7 +9,7 @@ let handler  = async (m, { conn, args }) => {
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
   if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (gimage = path.extname('index.bin')) { throw e; }
+  if (gimage = path.extname('.bin')) { return throw e; }
   conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
