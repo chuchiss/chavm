baedc0782895d6418e567d19e8bdcd6ea0c8fb75
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-05 03:33:45 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,12 +1,12 @@
 let handler = async (m, {user, conn}) => {
- // if (global.DATABASE._data.chats[m.chat].tutti === true) {
+ if (global.DATABASE._data.chats[m.chat].tutti == true) {
 
 
 
 //function resolveafte(){
 //setTimeout(() =>{
 function chelio(){
-conn.reply(m.chat,`
+return conn.reply(m.chat,`
 Con la letra:   “${picktRandom(global.tuttis)}” \n
 Nombre, apellido , cosas , animal , fruta , país o província\n
 [GOoooo] `, m)
@@ -15,12 +15,17 @@ Nombre, apellido , cosas , animal , fruta , país o província\n
 //}
 
 function chelo(){
-conn.reply(m.chat,`Listo para jugar al tutti frutti?\nPara leer las reglas escribe .reglas
+return conn.reply(m.chat,`Listo para jugar al tutti frutti?\nPara leer las reglas escribe .reglas
 cuando termines di basta!
-empieza en 20 segundos`, m)
+empieza en 20 segundos..`, m)
 }
 
+function lok(){
 global.DATABASE._data.chats[m.chat].tutti = false
+}
+function deslok(){
+global.DATABASE._data.chats[m.chat].tutti = true
+}
 
 //function resolveadd(){
 //return global.DATABASE._data.chats[m.chat].tutti = true
@@ -31,13 +36,15 @@ global.DATABASE._data.chats[m.chat].tutti = false
 
 
 //return{
-setTimeout(chelo, 3000)
-setTimeout(chelio, 1000)
+//setTimeout(lok, 500)
+setTimeout(chelo, 1000)
+setTimeout(chelio, 10000)
+//setTimeout(deslok, 15000)
 //}
 //asyncCall();
 //setTimeout(resolveadd, 2000);
 
-//  } else return m.reply('todavia estas en juego')
+// } else return m.reply('todavia estas en juego')
 }
 handler.help = ['tutti']
 handler.tags = ['game']
