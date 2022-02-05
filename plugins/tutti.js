f35bd1bef806c5799a569ad373c0358997dc6b2f
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-05 03:13:04 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -12,17 +12,17 @@ Con la letra:   “${picktRandom(global.tuttis)}” \n
 Nombre, apellido , cosas , animal , fruta , país o província\n
 [GOoooo] `, m)
 
-}, 8000);
+}, 1000);
 });
 }
 
 //m.reply('[bet£]Listo para jugar al tutti frutti?\nPara leer las reglas escribe .reglas
 //cuando termines di basta!
 //[empieza en 20 segundos]')
-return global.DATABASE._data.chats[m.chat].tutti = false
+// return global.DATABASE._data.chats[m.chat].tutti = false
 
 function resolveadd(){
-return global.DATABASE._data.chats[m.chat].tutti = true
+//return global.DATABASE._data.chats[m.chat].tutti = true
 }
 
 
@@ -32,7 +32,7 @@ async function asyncCall(){
 await resolveafte()
 }
 asyncCall();
-setTimeout(resolveadd, 2000);
+//setTimeout(resolveadd, 2000);
 
 //  } else return m.reply('todavia estas en juego')
 }
