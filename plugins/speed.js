d16aae47d3e9fca9dcf63eea97bc1be565aa3c8f
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 07:53:57 -0300

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -9,3 +9,10 @@ handler.tags = ['info', 'tools']
 
 handler.command = /^(ping|speed)$/i
 module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.funi = [
+  '1','2','3','4','5','6']
