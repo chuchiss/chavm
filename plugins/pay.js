8f9350c4f86004f7825c193f7fe367dd9c84c2f4
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 07:10:26 -0300

    Update pay.js

--- plugins/pay.js
+++ plugins/pay.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, text }) => {
   users[who].exp += xp
   users[m.sender].limit -= 1
   
-  m.reply(`Mรกs impuestos gastaste  *${-exp} XP* total ${result}`)
+  m.reply(`Mas impuestos gastaste  *${-exp} XP* tienes en total ${result}`)
   conn.fakeReply(m.chat, `te enviaron *+${xp} XP*`, who, m.text)
 }
 handler.help = ['pagar @user <amount>']
