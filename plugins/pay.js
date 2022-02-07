d7afcd5719f6c7ad529a30f96a8af909f54992ed
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 07:03:53 -0300

    Update pay.js

--- plugins/pay.js
+++ plugins/pay.js
@@ -9,19 +9,19 @@ let handler = async (m, { conn, text }) => {
   let txt = text.replace('@' + who.split`@`[0], '').trim()
   let xp = parseInt(txt)
   if (isNaN(xp)) return conn.reply(m.chat, `es .pagar @mension cantidad`, m)
-  let expe = xp
+  let exp = xp
   let pjk = Math.ceil(xp * pajak)
   exp += pjk
-let { exp} = global.DATABASE.data.users[m.sender].exp
-  if (expe < 50) return conn.reply(m.chat, `minimo 50`, m)
+ let result = user[m.sender].exp
+  if (exp < 50) return conn.reply(m.chat, `minimo 50`, m)
   let users = global.DATABASE._data.users
-  if (expe > users[m.sender].exp) return conn.reply(m.chat, `no tenes esa exp`, m)
+  if (exp > users[m.sender].exp) return conn.reply(m.chat, `no tenes esa exp`, m)
  if (users[m.sender].limit < 1) return conn.reply(m.chat, `se requiere un limit m?nimo`, m)
-  users[m.sender].exp -= expe
+  users[m.sender].exp -= exp
   users[who].exp += xp
   users[m.sender].limit -= 1
   
-  m.reply(`Más impuestos gastaste  *${-expe} XP* total ${exp}`)
+  m.reply(`Mรกs impuestos gastaste  *${-exp} XP* total ${result}`)
   conn.fakeReply(m.chat, `te enviaron *+${xp} XP*`, who, m.text)
 }
 handler.help = ['pagar @user <amount>']
