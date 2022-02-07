8752df255df9237a0b6c26084979572c6e386434
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 14:23:54 -0300

    Update luck.js

--- plugins/luck.js
+++ plugins/luck.js
@@ -9,7 +9,7 @@ let result = Math.ceil(xp * pajak)
 global.DATABASE._data.users[m.sender].exp += result
 global.DATABASE._data.users[m.sender].limit -= 10
 let chelo = (resulta + result)
-m.reply(`*Ganaste* *${result}*\n  - _Tenias un total de_ ${resulta} \n -- _Ahora tienes_ ${chelo}`)q
+conn.reply(m.chat, `*Ganaste* *${result}*\n  - _Tenias un total de_ ${resulta} \n -- _Ahora tienes_ ${chelo}`, m)
 }
 function prueba(){
 conn.reply(m.chat, `Prueba tu suerte check \n Gana hasta 30 mil exp \n Costo 10 limites\n \n cargando....`, m)
