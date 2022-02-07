6c41b9d5e039f9e480117d50635b0c1a9de8962b
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 14:19:08 -0300

    Update luck.js

--- plugins/luck.js
+++ plugins/luck.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-
+function chuqui(){
 if (global.DATABASE._data.users[m.sender].limit < 10) return m.reply('No tienes mas limites para comprar pon .buy')
 let pajak = Math.random()
 let xp = 30000
@@ -8,7 +8,10 @@ let result = Math.ceil(xp * pajak)
 global.DATABASE._data.users[m.sender].exp += result
 global.DATABASE._data.users[m.sender].limit -= 10
 let chelo = (resulta + result)
-m.reply(`*Ganaste* *${result}*\n  - _Tenias un total de_ ${resulta} \n -- _Ahora tienes_ ${chelo}`)
+m.reply(`*Ganaste* *${result}*\n  - _Tenias un total de_ ${resulta} \n -- _Ahora tienes_ ${chelo}`)q
+}
+m.reply('Prueba tu suerte check \n Gana hasta 30 mil exp \n Costo 10 limites\n \n cargando....')
+setTimeout(chiqui, 12000)
 }
 handler.help = ['luck', 'suerte']
 handler.tags = ['game']
