c3220a7a1a0d87c8cd68553de1cb7da11d38d232
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 13:31:16 -0300

    Create luck.js

---
+++ plugins/luck.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn }) => {
+
+let pajak = Math.random()
+let xp = 100000
+let resulta = global.DATABASE.data.users[m.sender].exp
+let result = Math.ceil(xp * pajak)
+global.DATABASE._data.users[m.sender].exp += result
+global.DATABASE._data.users[m.sender].limit -= 10
+m.reply(`Ganaste ${result}\n  _tienes un total de_ ${resulta}`)
+}
+handler.help = ['luck', 'suerte']
+handler.tags = ['game']
+handler
+handler.group = true
+handler.command = /^(luck|suerte)$/i
+module.exports = handler
