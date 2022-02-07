2fba5dfbc3d0e7001aac8081b471f2f67642608d
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 08:05:10 -0300

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -3,14 +3,16 @@ let handler = async (m, { conn }) => {
   let old = performance.now()
   let neww = performance.now()
   m.reply(neww - old + 'ms')
+
 let ero = pickRandom(global.funi)
-if (ero > 3) m.reply('cheko')
+if (ero > 3) conn.sendFile(m.chat, 'media/esu.jpg', 'jpg', null, m, true)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
 
 handler.command = /^(ping|speed)$/i
 module.exports = handler
+
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
