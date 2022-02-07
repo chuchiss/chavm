59d567c77396f16f47c14981ce49860230436411
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 13:01:45 -0300

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -5,7 +5,8 @@ let handler = async (m, { conn }) => {
   m.reply(neww - old + 'ms')
 
 
-m.reply('${pickRandom(global.culo)}')
+m.reply(`${x}`)
+let x = `${pickRandom(global.culo)}`
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
