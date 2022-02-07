1f32ce0ccf7990302c2a30ad4a9472221b9936a3
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 10:49:16 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,7 +1,7 @@
 let handler = async (m, {user, conn}) => {
 let chencho = global.DATABASE._data.chats[m.chat]
 let chenchu = global.DATABASE._data.users[m.sender]
- if(chencho.jog < 2){ conn.reply(m.chat, `EsperNdo contrincante..`, m) 
+if (chencho.jog < 2){ conn.reply(m.chat, `EsperNdo contrincante..`, m) 
 chencho.jog += 1
 chenchu.mismo += 1
 return 0!
