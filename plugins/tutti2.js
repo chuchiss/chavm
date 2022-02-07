8026806125115df1397220110026f0d8aa8dea82
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 10:50:10 -0300

    Update and rename tutti2 to tutti2.js

--- plugins/tutti2.js
+++ plugins/tutti2.js
@@ -8,13 +8,14 @@ let handler = async (m, { conn, text }) => {
 //  if (exp < 50) return conn.reply(m.chat, `minimo 50`, m)
   let users = global.DATABASE._data.users
  let chats = global.DATABASE._data.chats
-//  if (chats[m.chat].jog < 1){ return conn.reply(m.chat, `no estss ennjuego`, m)
+//if (chats[m.chat].jog < 1){ return conn.reply(m.chat, `no estss ennjuego`, m)
 //users[m.sender].point -= puntos}
-if (users[m.sender].point > 100) { return conn.reply(m.chat, `finalizado Ganaste +500 exp`, m)
+if (users[m.sender].point > 100) { conn.reply(m.chat, `finalizado Ganaste +500 exp`, m)
 users[m.sender].exp += 500
 users[m.sender].point -= puntos 
 chats[m.chat].jog = 0
 users[m.sender].mismo = 0
+return 0!
 }
   users[m.sender].point += puntos
 
