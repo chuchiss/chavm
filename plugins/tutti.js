2cc286c094e85b2f4e0060da45b569218fdf7381
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 04:36:02 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -69,4 +69,4 @@ function picktRandom(list) {
 
 
 global.tuttis = [
-'a','b','c','d','e','f','g']
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
