f6ac5d828f20e3df246b6753e937e7842c1d3545
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 04:54:42 -0300

    Update infotutti.js

--- plugins/infotutti.js
+++ plugins/infotutti.js
@@ -1,1 +1,27 @@
+let handler = async (m, {conn}) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastme > 5400) {
+m.reply(`
+el primero que completa todo debe decir “Tutti Frutti”, 
+y de inmediato, los demás deben enviar el mensaje
+por donde se quedaron en completar las características. 
+Luego, 
+viene la puntuación: 
+· Palabras válidas escritas  se asignan 10 puntos
+· Las palabras repetidas, se asignan 5 puntos
+ si te falto 2 o 3 características también puedes cantar tutti fruti
 
+
+también puedes apostar exp, jugar por prendas. etc. 
+Que cante Tutti fruti el mejor, a jugar!!
+*___________________________________*
+  ȼøᵽɏɍɨǥħŧ©ɨnsȺnɇƀøŧ.øfɨȼɨȺł
+                                        
+`.trim()) 
+global.DATABASE._data.users[m.sender].lastme = new Date * 1
+  } else return
+}
+handler.help = ['it (instrucciones tutti)']
+handler.tags = ['info']
+handler.command = /^(it)$/i
+
+module.exports = handler
