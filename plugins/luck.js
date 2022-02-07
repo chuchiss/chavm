8a34c9f2df415ea0e5b0d594795e473085a54741
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 13:39:04 -0300

    Update luck.js

--- plugins/luck.js
+++ plugins/luck.js
@@ -2,12 +2,13 @@ let handler = async (m, { conn }) => {
 
 let pajak = Math.random()
 let xp = 50000
+let chumchi = if (result > 20){ 😳😳 }
 let resulta = global.DATABASE.data.users[m.sender].exp
 let result = Math.ceil(xp * pajak)
 global.DATABASE._data.users[m.sender].exp += result
 global.DATABASE._data.users[m.sender].limit -= 6
 let chelo = (resulta + result)
-m.reply(`Ganaste ${result}\n  _tenias un total de_ ${resulta} \n Ahora tienes ${chelo}`)
+m.reply(`Ganaste ${result} ${chumchi}\n  _tenias un total de_ ${resulta} \n Ahora tienes ${chelo}`)
 }
 handler.help = ['luck', 'suerte']
 handler.tags = ['game']
