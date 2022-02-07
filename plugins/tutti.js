0c2b41f5cf510c0722c1a93ad9c84a4ea425acdb
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 06:35:27 -0300

    Update tutti.js

--- plugins/tutti.js
+++ plugins/tutti.js
@@ -1,15 +1,16 @@
 let handler = async (m, {user, conn}) => {
 let chencho = global.DATABASE._data.chats[m.chat]
-let chenchu = global.DATABASE._data.users[m.sender]
-if (chencho.tutti < 1 & chencho.jog <= 2){ return {conn.reply(m.chat, `EsperNdo contrincante..`, m) 
-chencho.jog += 1
-chenchu.mismo += 1}
-}
-if (chenchu.mismo >= 1) {return}
+ if (chencho.tutti >= 0) {
 
+
+//function resolveafte(){
+//setTimeout(() =>{
+
+//}, 1000)
+//}
 
 function chelo(){
-return conn.reply(m.chat,`Listo para jugar al tutti frutti?\nPara leer las instrucciones escribe .it
+return conn.reply(m.chat,`Listo para jugar al tutti frutti?\nPara leer las reglas escribe .reglas
 cuando termines di basta!
 empieza en 20 segundos..`, m)
 }
@@ -24,19 +25,31 @@ Nombre, apellido , cosas , animal , fruta , país o província\n
 
 
 function lok(){
-return chencho.tutti += 1
-}
-function deslok(){
 return chencho.tutti -= 1
 }
+function deslok(){
+return chencho.tutti += 1
+}
 
+//function resolveadd(){
+//global.DATABASE._data.chats[m.chat].tutti == true
+//}
+
+
+
+
+
+//return{
 
 setTimeout(lok, 100)
 setTimeout(chelo, 1000)
 setTimeout(chelio, 10000)
 setTimeout(deslok, 40000)
+//}
+//asyncCall();
+//setTimeout(resolveadd, 2000);
 
-} else return m.reply('todavia estas en juego.. \espera unos segundos..')
+ } else return m.reply('todavia estas en juego.. \n espera unos segundos..')
 }
 handler.help = ['tutti']
 handler.tags = ['game']
