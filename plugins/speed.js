a6b360ada4ab4f28207664e919b7ecbb6fc2dabf
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 07:47:42 -0300

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -3,8 +3,6 @@ let handler = async (m, { conn }) => {
   let old = performance.now()
   let neww = performance.now()
   m.reply(neww - old + 'ms')
-var aleatorio = Math.random()
-m.reply(aleatorio)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
