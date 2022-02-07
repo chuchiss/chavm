d55dffddb9a89f04dea5cc9aea73fb3b5ebc659f
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-07 12:09:08 -0300

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let old = performance.now()
   let neww = performance.now()
   m.reply(neww - old + 'ms')
-
+m.reply(`${number}`)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
@@ -12,3 +12,5 @@ handler.command = /^(ping|speed)$/i
 module.exports = handler
 
 
+let myNumeroAleatorio = Math.floor(Math.random()*(200+1))
+let number = pickRandom(myNumeroAleatorio)
