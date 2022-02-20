83baeac6c728f52414d02a11627c3ea40fa0416a
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-20 08:54:47 -0300

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -6,6 +6,7 @@ let handler  = async (m, { conn, text, args, usedPrefix, command, isLimit, isPre
 	if (!args || !args[0]) return conn.reply(m.chat, `No es así!\n\n*tenes que poner* : _${usedPrefix + command} Hola_`, m)
         
 	//let text = args.join ` `
+        let text = text
 	fetch("https://api.simsimi.net/v2/?text=" + encodeURIComponent(text) + "&lc=es")
   .then(res => res.json())
   .then(batch => {
