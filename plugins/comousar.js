3e591ab8e1024c7948b0d25a6075474832a0a7ae
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-02-20 09:23:06 -0300

    Update comousar.js

--- plugins/comousar.js
+++ plugins/comousar.js
@@ -1,10 +1,18 @@
 let handler = async m => m.reply(`
 Hola para usar este servicio
-tenes que escribir los comando poniendo un 
-puntito ante cada palabra
+tenes que escribir los comandos poniendo un 
+puntito y el comando que quieras utilizar
 
-ejemplo " .s texto "
+ejemplo " .simi texto "
+ejemplo " .simi hola como estas "
+ejemplo " .simi que haces? "
  sin las comillas es para usar simsimi
+
+otros ejemplos:
+
+para crear un sticker debes 
+seleccionar o subir una imagen
+luego escribir " .sticker " sin las comillas
 
 para saber los comandos pone .comandos
 para saber todos los comandos pone .menu
