9f769a71c1e07996890b41934e633eee9e697520
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-02 18:06:55 -0300

    Create primero.js

---
+++ plugins/primero.js
@@ -0,0 +1,68 @@
+let handler = async (m, {user, conn}) => {
+let chencho = global.DATABASE._data.chats[m.chat]
+ if (chencho.tutti >= 0) {
+
+
+//function resolveafte(){
+//setTimeout(() =>{
+
+//}, 1000)
+//}
+
+function chelo(){
+return conn.reply(m.chat,`EL PRIMERO EN DECIR LA PALABRA GANA!
+empieza en 20 segundos..`, m)
+}
+
+
+function chelio(){
+return conn.reply(m.chat,`
+La palabra:   “${picktRandom(global.tuttis)}” \n
+`, m)
+}
+
+
+function lok(){
+return chencho.tutti -= 1
+}
+function deslok(){
+return chencho.tutti += 1
+}
+
+//function resolveadd(){
+//global.DATABASE._data.chats[m.chat].tutti == true
+//}
+
+
+
+
+
+//return{
+
+setTimeout(lok, 100)
+setTimeout(chelo, 1000)
+setTimeout(chelio, 10000)
+setTimeout(deslok, 40000)
+//}
+//asyncCall();
+//setTimeout(resolveadd, 2000);
+
+ } else return m.reply('todavia estas en juego.. \n espera unos segundos..')
+}
+handler.help = ['primero']
+handler.tags = ['game']
+handler.command = /^(primero)$/i
+
+module.exports = handler
+
+
+
+
+
+function picktRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+
+global.tuttis = [
+'gorra','pescado','mandarina','licuado','anonimo','confianza','transiccion','conexion','cohete','jujuy','vivienda','tecnologia','caminata','helicoptero','oso','puerta','quincho','rucula','sopa','tigre','uva','vampiro','washington','politica']
