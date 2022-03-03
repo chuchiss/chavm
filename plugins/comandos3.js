dccc049f7af5a54ecb39478cc631a9902742a79a
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-03 04:35:10 -0300

    Update comandos3.js

--- plugins/comandos3.js
+++ plugins/comandos3.js
@@ -1,23 +1,29 @@
 let handler = async (m, {conn}) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastme > 5400) {
 m.reply(`
-        ➿🄹🅄🄴🄶🄾🅂
-_.mate_ medium (ganar exp)
-_.adiv_ start (ganar exp)
-_.tateti_
-_.tutti_ (beta)
-_.luck_
-_.palabra_ (beta)
-_proximo a salir preguntados..._
+        ➿ADMIN
+_.chau_  (seleccionando o etiquetando)
+_.kick_ (quitea usuario)
+_.hablen_ (abre grupo)
+_.callense_ (cerrar grupo)
+_.grupo_ (cambia de nombre el grupo)
+_.descripcion_  (cambia descripción del grupo)
+_.enable_ (activar opciones de grupo)
+_.disable_ (desactivar opciones de grupo)
+_.tagall_ (premium)
+_.sider_ (ausentes en el grupo)
+_.online_ (quien está online en el grupo)
+
+
 *___________________________________*
-  ȼøᵽɏɍɨǥħŧ©ɨnsȺnɇƀøŧ.øfɨȼɨȺł
+         ȼøᵽɏɍɨǥħŧ©ɨnsȺnɇ
                                         
 `.trim()) 
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
   } else return
 }
-handler.help = ['juegos']
+handler.help = ['admin]
 handler.tags = ['info']
-handler.command = /^(juegos|game)$/i
+handler.command = /^(admin|adm)$/i
 
 module.exports = handler
