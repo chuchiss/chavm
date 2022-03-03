5d6e7f928b309004fc73f13df4877860d2ec0ad3
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-03 04:40:11 -0300

    Update comandos4.js

--- plugins/comandos4.js
+++ plugins/comandos4.js
@@ -7,15 +7,13 @@ _.kick_ (quitea usuario)\n
 _.adv_ (alerta de advertencia a usuario)\n
 _.hablen_ (abre grupo)\n
 _.callense_ (cerrar grupo)\n
-_.grupo_ (cambia de nombre el grupo)
-_.descripcion_  (cambia descripción del grupo)
-_.enable_ (activar opciones de grupo)
+_.grupo_ (cambia de nombre el grupo)\n
+_.descripcion_  (cambia descripción del grupo)\n
+_.enable_ (activar opciones de grupo)\n
 _.disable_ (desactivar opciones de grupo)\n
 _.tagall_ (premium)\n
 _.sider_ (ausentes en el grupo)\n
 _.online_ (quien está online en el grupo)\n
-
-
 *___________________________________*
          ȼøᵽɏɍɨǥħŧ©ɨnsȺnɇ
                                         
