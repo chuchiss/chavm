ebf31f2da5443a5bf03e69b6962c0f3b50cc8122
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-05 19:06:49 -0300

    Update twitear.js

--- plugins/twitear.js
+++ plugins/twitear.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix }) => {
     conn.twit = conn.twit ? conn.twit : {}
     let id = m.all
-    if (!(id in conn.twit)) conn.twit[id][0].push(m.text)
+    if (id in conn.twit) conn.twit[id][0].push(m.text)
      if (/chat.whatsapp.com\/([0-9A-Za-z]{20,24})/.exec(m.text)) return
     if (!text) return m.reply('twit vacío!  .twittear esto es un ejemplo')
     m.reply(`¡twuiteado !\n*${usedPrefix}twits  para ver los twits`)
