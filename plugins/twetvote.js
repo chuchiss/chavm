51d3d353abf79c659236e3a0d21ca1fd40f477c1
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-05 17:23:18 -0300

    Update twetvote.js

--- plugins/twetvote.js
+++ plugins/twetvote.js
@@ -15,5 +15,5 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['megusta', 'nomegusta']
 handler.tags = ['vote']
 handler.command = /^(megusta|nomegusta)$/i
-handler.group = true
+handler.group = false
 module.exports = handler
