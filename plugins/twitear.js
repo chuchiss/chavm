b904ee0a9f923bfa1f4760faedd2feacd2c77ce7
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-05 16:15:19 -0300

    Update twitear.js

--- plugins/twitear.js
+++ plugins/twitear.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
-    
+    let id = m.all
     m.reply(`¡twuiteado !\n*${usedPrefix}si* - para si\n*${usedPrefix}no* - para no\n*${usedPrefix}chekear* - para chekear el voto\n*${usedPrefix}borrarvotacion* - para eliminar`)
     conn.vote[id] = [
         text,
