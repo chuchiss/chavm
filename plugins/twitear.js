fd158143235ad90327cc3205adbb266ecab05603
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-06 00:23:48 -0300

    Update twitear.js

--- plugins/twitear.js
+++ plugins/twitear.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isPrems, isOwner, text, user, usedPrefix }) => {
     conn.twit = conn.twit ? conn.twit : {}
     let id = m.all
     if ( global.DATABASE._data.users[m.sender].limit < 1) return m.reply('minimo 10 limits')
-     if (/https\/([0-9A-Za-z]{20,24})/.exec(m.text)) return
+     if text = (([0-9A-Za-z]{20,24})/.exec(m.text)) return
     if (!text) return m.reply('twit vacío!  .twittear esto es un ejemplo')
     m.reply(`¡twuiteado !\n*${usedPrefix}twits  para ver los twits`)
     conn.twit[id] = [
