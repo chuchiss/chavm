0e4c57d818ca08bc50101c422da23de1cfee49e8
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-06 02:58:03 -0300

    Update twits.js

--- plugins/twits.js
+++ plugins/twits.js
@@ -1,8 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let id = m.all
     conn.twit = conn.twit ? conn.twit : {}
-    
-
+   
     let [reason, si, no] = conn.twit[id]
     let mentionedJid = [ ...si, ...no]
     m.reply(`
