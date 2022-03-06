9fd5c6fb4291170fd9bad295f6d2042f2cdfee86
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-06 02:16:03 -0300

    Update twitear.js

--- plugins/twitear.js
+++ plugins/twitear.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isPrems, isOwner, text, user, usedPrefix }) => {
     conn.twit = conn.twit ? conn.twit : {}
     let id = m.all
     if ( global.DATABASE._data.users[m.sender].limit < 1) return m.reply('minimo 10 limits')
-     if (text in conn.vote[id]) (/chat.whatsapp.com\/([0-9A-Za-z]{20,24})/.exec(m.text)) return
+     if (text in conn.twit[id]) (/chat.whatsapp.com\/([0-9A-Za-z]{20,24})/.exec(m.text)) return
     if (!text) return m.reply('twit vacío!  .twittear esto es un ejemplo')
     m.reply(`¡twuiteado !\n*${usedPrefix}twits  para ver los twits`)
     conn.twit[id] = [
@@ -12,6 +12,9 @@ let handler = async (m, { conn, isPrems, isOwner, text, user, usedPrefix }) => {
         []
     ]
 if (!isPrems && !isOwner) global.DATABASE._data.users[m.sender].limit -= 7
+    setTimeout(()=> {
+delete conn.twit[id]
+}, 2000)
 }
 handler.help = ['twitear (que esta pasando)']
 handler.tags = ['vote']
