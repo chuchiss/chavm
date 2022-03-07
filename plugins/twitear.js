aa930b48e63038c5ccad3c6d075a41e09579f9eb
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-06 23:59:54 -0300

    Update twitear.js

--- plugins/twitear.js
+++ plugins/twitear.js
@@ -1,11 +1,9 @@
 let handler = async (m, { args, conn, isPrems, isOwner, text, user, usedPrefix }) => {
     conn.twit = conn.twit ? conn.twit : {}
     let id = m.all
-    let text = args.join ` `
-    if(args[0].length > 20) {
-	return conn.reply(m.chat, `twit max. 20 carácteres!`, m)
     if ( global.DATABASE._data.users[m.sender].limit < 30) return m.reply('minimo 30 limits disponibles para tw - cada tw cuesta 6 + iva')
     if (!text) return m.reply('twit vacío!  .twittear esto es un ejemplo')
+    if (text > 18) return conn.reply(m.chat, 'Lo siento, texto demasiado largo, máximo 18 caracteres!', m)
     m.reply(`¡twuiteado !\n*${usedPrefix}twits  para ver los twits`)
     conn.twit[id] = [
         text,
