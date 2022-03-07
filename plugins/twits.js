a22d5ce5c4c9c98421e0b4b6b91daaa2a423d1c8
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-06 22:39:25 -0300

    Update twits.js

--- plugins/twits.js
+++ plugins/twits.js
@@ -1,8 +1,6 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let id = m.all
     conn.twit = conn.twit ? conn.twit : {}
-    
-
     let [reason, si, no] = conn.twit[id]
     let mentionedJid = [ ...si, ...no]
     m.reply(`
@@ -23,5 +21,5 @@ handler.help = ['twitear']
 handler.tags = ['vote']
 handler.command = /^(twit|twits|(list|ver)twits)$/i
 handler.group = false
-handler.owner = true
+
 module.exports = handler
