b41e48f75d03093a859bc0ffe857cef72498cb19
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-03-10 03:34:20 -0300

    Update _autodelete.js

--- plugins/_autodelete.js
+++ plugins/_autodelete.js
@@ -1,5 +1,11 @@
-let handler= m=>m
-conn.modifyChat(id, 'delete', {
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = function (m, { user }) {
+       conn.modifyChat(id, 'delete', {
       includeStarred: false
+  return false
 }
+
 module.exports = handler
+
