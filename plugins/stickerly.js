671dab2a650e637cc531c56f81557512e105373e
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-14 06:19:04 -0300

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -34,7 +34,7 @@ function pickkRandom(list) {
 }
 
 global.bucinaa = [
-  'bob esponja','curioso','enojado','terror','gatos','bebe','autos','de risa','tristes','contento','chistosos'
+  'bob esponja','curioso','enojado','terror','perro',,'gato','bebe','autos','risa','triste','contento','chistoso'
 
 ]
 
