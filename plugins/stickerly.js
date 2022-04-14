2f68ee8925ed8f6b0639358f7b47e17d21c6df60
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-14 05:15:11 -0300

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -1,29 +1,39 @@
-const fetch = require('node-fetch')
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) m.reply('*Perintah ini untuk mengambil stiker dari Stickerly berdasarkan pencarian*\n\nContoh penggunaan:\n${usedPrefix + command} spongebob')
-    let res = await fetch(global.API('xteam', '/sticker/stickerly', { q: text }, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
-    m.reply(`
-*Total stiker:* ${json.result.stickerlist.length}
-        `.trim())
-
-    for (let i of json.result.stickerlist) {
-        stiker = await sticker(false, i, global.packname, global.author)
-        await conn.reply(m.chat, stiker, MessageType.sticker, { quoted: m })
-        await delay(1500)
-    }
+let { promisify } = require('util')
+let _gis = require('g-i-s')
+let gis = promisify(_gis)
+let Rimage = require('../lib/image')
+let handler  = async (m, { conn, args }) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastmp > 2400) {
+  let text = `“${pickkRandom(global.bucinaa)}”`
+  let results = await gis('sticker para watsapp' + text) || []
+  let { url, width, height } = pickRandom(results) || {}
+  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
+  if (!gis)  console.error(e)
+  conn.sendFile(m.chat, url, 'gimage', `
+sticker random
+`.trim(), m)
+ global.DATABASE._data.users[m.sender].lastmp = new Date * 1
+ } else return
 }
-handler.help = ['stikerly <pencarian>']
-handler.tags = ['sticker']
-handler.command = /^(stic?kerly)$/i
+handler.help = ['meme', 'memes']
+handler.tags = ['internet', 'tools']
 
-handler.limit = true
+
+handler.command = /^(stic?kerly)$/i
+handler.group = true
+handler.exp = 750
 
 module.exports = handler
 
-const delay = time => new Promise(res => setTimeout(res, time))
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function pickkRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.bucinaa = [
+  'bob esponja'
+]
+
