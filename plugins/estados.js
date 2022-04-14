a154f488bd5b0595467e29acb0c764d3724c1c59
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-14 06:33:28 -0300

    Update estados.js

--- plugins/estados.js
+++ plugins/estados.js
@@ -9,8 +9,8 @@ let handler = async (m, { users, conn, command, isPrems, isOwner }) => {
   let results = await yts('estado para Whatsapp'+ textu + texti)
   let vid = results.all.find(video => video.seconds < 3600)
   if (!vid) return conn.reply(m.chat, `video o audio no encontrado`, m)
-  let isVideo = /ados$/.test(command)
-  let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
+  let isVideo = /2$/.test(command)
+  let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? yta : ytv)(vid.url, 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 ${title}
@@ -19,16 +19,16 @@ ${isLimit ? ' ': ''}
   let _thumb = {}
   try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { return }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /ados$/.test(command)), `
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
 ${title}
 `.trim(), m, false, _thumb || {})
 global.DATABASE._data.users[m.sender].lastmp = new Date * 1
 if (!isPrems && !isOwner) global.DATABASE._data.users[m.sender].limit -= 2
   } else m.reply('Cargando..')
 }
-handler.help = ['random'].map(v => v + ' <canción random>')
+handler.help = ['random'].map(v => v + ' <estados random>')
 handler.tags = ['downloader']
-handler.command = /^estados?$/i
+handler.command = /^estados2?$/i
 handler.group = true
 handler.limit = true
 
