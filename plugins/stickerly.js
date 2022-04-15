eb7d372d8d1dbbaca77d3d4de36d6d3876a2255e
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-15 08:42:32 -0300

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -33,7 +33,7 @@ function pickkRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 
-global.bucinaa = [ 'sponge bob','pocoyo','tom and jerry','shreck','bart simpsons','spiderman'
+global.bucinaa = [ 'sponge bob','pocoyo','tom and jerry','shreck','bart simpsons','spiderman','gavin','grand m','lohee'
 ]
 
 global.bucinaas = [
