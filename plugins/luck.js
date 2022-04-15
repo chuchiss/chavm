e3a7a803bc1d307ec0b2e48262842f266eb6e73c
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-15 05:26:57 -0300

    Update luck.js

--- plugins/luck.js
+++ plugins/luck.js
@@ -9,9 +9,10 @@ let result = Math.ceil(xp * pajak)
 global.DATABASE._data.users[m.sender].exp += result
 global.DATABASE._data.users[m.sender].limit -= 10
 let chelo = (resulta + result)
+let tocno = global.DATABASE._data.users[m.sender].limit
 let chenchy = (result - 5000)
-if (result < 5000) return conn.reply(m.chat, `*Perdiste* ${-chenchy} xp\n - _Tenias un total de_ ${resulta} \n -- _Ahora tienes_ ${chelo}`, m)
-conn.reply(m.chat, `*Ganaste* *${chenchy}* xp \n  - _Tenias un total de_ ${resulta} xp \n -- _Ahora tienes_ ${chelo} \n xp`, m)
+if (result < 5000) return conn.reply(m.chat, `*Perdiste* ${-chenchy} xp\n - _Tenias un total de_ ${resulta} xp\n -- _Ahora tienes_ ${chelo} xp \n Limites totales ${tocno}`, m)
+conn.reply(m.chat, `*Ganaste* *${chenchy}* xp \n  - _Tenias un total de_ ${resulta} xp \n -- _Ahora tienes_ ${chelo} xp \n Limite totales ${tocno}`, m)
 }
 function prueba(){
 conn.reply(m.chat, `*Prueba tu suerte check*🤙 \n ▫️Gana hasta el doble de exp \n ▫️Costo 10 limites\n \n Cargando....`, m)
