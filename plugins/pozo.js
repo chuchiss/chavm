a4fb5aab7010f0c25b4372b5152ccc58f55d1dcb
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-15 21:49:57 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -1,24 +1,30 @@
-let handler = async (m, { conn, users }) => {
+let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastmp > 12000) {
-function chuquii(){
-
-conn.reply(m.chat, `perdiste` m)
+function chuqui(){
+if (global.DATABASE._data.users[m.sender].limit < 10) return m.reply('No tienes mas limites para comprar pon .buy')
+let pajak = Math.random()
+let xp = 10000
+let resulta = global.DATABASE.data.users[m.sender].exp
+let result = Math.ceil(xp * pajak)
+global.DATABASE._data.users[m.sender].exp += result
+global.DATABASE._data.users[m.sender].limit -= 10
+let chelo = (resulta + result)
+let tocno = global.DATABASE._data.users[m.sender].limit
+let chenchy = (result - 5000)
+if (result < 5000) return conn.reply(m.chat, `*Perdiste* ${-chenchy} xp\n - _Tenias un total de_ ${resulta} xp\n -- _Ahora tienes_ ${chelo} xp \n --- _Limites totales_ ${tocno}`, m)
+conn.reply(m.chat, `*Ganaste* *${chenchy}* xp \n - _Tenias un total de_ ${resulta}xp\n -- _Ahora tienes_ ${chelo} xp \n --- _Limite totales_ ${tocno}`, m)
 }
-function pruebaa(){
-conn.reply(m.chat, `*Pozo acumulado \n ▫️pozo minimo ${costo} \n total de veces intentado ${veces} \n \n Cargando....`, m)
+function prueba(){
+conn.reply(m.chat, `*Prueba tu suerte check*🤙 \n ▫️Gana hasta el doble de exp \n  ▫️ Costo 10 limites \n \n Cargando....`, m)
 }
-setTimeout(pruebaa, 1000)
-setTimeout(chuquii, 12000)
+setTimeout(prueba, 1000)
+setTimeout(chuqui, 12000)
 global.DATABASE._data.users[m.sender].lastmp = new Date * 1
   } else return
 }
-handler.help = ['pozo']
+handler.help = ['luck']
 handler.tags = ['game']
 handler.limits = true
 handler.group = true
-handler.command = /^pozo$/i
+handler.command = /^(pozo)$/i
 module.exports = handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
