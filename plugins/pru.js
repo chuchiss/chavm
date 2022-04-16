740ceb534012bc1bd87091dc86cb92ea431e6f3a
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 06:16:56 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn, text}) => {
 text1 = ''
 text2 = ''
 }
-global.lolo.text2 = text
+if (!text) global.lolo.text2 = text
 if (isNaN) global.lolo.text1 = text
 m.reply(` ${global.lolo.text1}
 ${global.lolo.text2}                            
