357eb4ffdb346c8963badd6f1094efe6fb299eee
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 00:36:27 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -15,13 +15,13 @@ function ttero() {
   return list[Math.floor(list.length * Math.random())]
 }
  global.DATABASE.data.chats[m.chat].pozo += 1
-  if (ero > 5) { conn.reply(m.chat, `ganaste`, m)
+  if (ero > 5) { conn.reply(m.chat, `FELICIDADES!! Haz ganado el pozo de ${lukia} xp!!!`, m)
  global.DATABASE.data.chats[m.chat].pozo -= veces
 return
 }
- conn.reply(m.chat, `perdiste, Intentalo de nuevo!`, m)
+ conn.reply(m.chat, `*∆perdiste* , Intentalo de nuevo!`, m)
 }
-setTimeout(ttero, 3500)
+setTimeout(ttero, 8500)
 
 }
 handler.help = ['pru']
