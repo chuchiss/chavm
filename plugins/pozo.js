6de8256d262c77f90e65b7d134ab2d4bfeb7a259
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 06:41:42 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -41,4 +41,4 @@ handler.command = /^(pozo)$/i
 module.exports = handler
 
 
-global.funzi ['1','2','3','4','5','6','7','8','9','10','11','12']
+global.funzi = ['1','2','3','4','5','6','7','8','9','10','11','12']
