add7a9096707dd871e1541d83f3e92054a39d459
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 06:20:30 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -5,8 +5,8 @@ text2 = ''
 }
 if (!text) global.lolo.text2 = text
 if (isNaN) global.lolo.text1 = text
-m.reply(` ${global.lolo.text1}
-${global.lolo.text2}                            
+m.reply(` 
+${global.lolo.text1.push(m.text)}                            
 `.trim()) 
 
 }
