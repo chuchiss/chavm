8bd2484e6d757f779a383d867e81dbbf7e6d0a5f
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 04:26:11 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
 function mmero() {
   let costo = 5000
   let veces = global.pozo
+  if(typeof global.pozo == 'undefined') {
   global.pozo = 0
-  if (!isNumber(global.pozo)) global.pozo = 0
-  
+  }
   let lukia = (costo * veces)
   conn.reply(m.chat, `pozo acumulado \ntotal del pozo ${lukia}  \ntotal de intentos ${veces}`, m)
 }
