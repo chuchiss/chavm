8c0d10eba32d2ad4aed1763f4ffd4a131a5d40c5
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-16 07:21:39 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -14,13 +14,12 @@ function ttero() {
  let costo = 5000
   let veces = global.pozo
   let lukia = (costo * veces)
- let ero = pickRandom(global.funzi)
- function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
+let pajak = Math.random()
+let xp = 10
+let elo = Math.ceil(xp * pajak)
 let cilu = global.DATABASE._data.users[m.sender]
 if (cilu.exp < costo) return conn.reply(m.chat, `*Lo* *siento* , _No tienes suficiente Exp!!_ `, m)
-if (ero == 34) { conn.reply(m.chat, `🤑*∆FELICIDADES!! \nHaz ganado el pozo de ${lukia} xp!!!`, m)
+if (elo == 4) { conn.reply(m.chat, `🤑*∆FELICIDADES!! \nHaz ganado el pozo de ${lukia} xp!!!`, m)
 cilu.exp += lukia
 global.pozo -= veces
 return
@@ -41,4 +40,4 @@ handler.command = /^(pozo)$/i
 module.exports = handler
 
 
-global.funzi = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','45','46','47','48','49','50']
+global.funzi = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86''87','88','89','90','91','92]
