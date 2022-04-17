814989ad8404c6873dddfd694d4215875016de3a
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-17 13:36:49 -0300

    Update pozo.js

--- plugins/pozo.js
+++ plugins/pozo.js
@@ -5,12 +5,12 @@ let costo = 500
 let veces = global.pozo
 let lukia = (costo * veces)
 let pajak = Math.random()
-let xp = 4000
+let xp = 50
 let elo = Math.ceil(xp * pajak)
 let cilu = global.DATABASE._data.users[m.sender]
 if (cilu.limit < 2) return conn.reply(m.chat, `*Lo* *siento* , _Minimo 1 limit!!_ `, m)
 if (cilu.exp < costo) return conn.reply(m.chat, `*Lo* *siento* , _No tienes suficiente Exp!!_ `, m)
-if (elo == 3187) { conn.reply(m.chat, `🤑*∆FELICIDADES!! \nHaz ganado el pozo de ${lukia} xp!!!`, m)
+if (elo == 33) { conn.reply(m.chat, `🤑*∆FELICIDADES!! \nHaz ganado el pozo de ${lukia} xp!!!`, m)
 cilu.exp += lukia
 global.pozo -= veces
 return
