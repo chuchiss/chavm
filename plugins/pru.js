61fecd62ff4321e5d48fa63b8f9d719aa267a2a4
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-17 13:12:38 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -6,12 +6,18 @@ global.logi = [ 'funda','cepillo','coron'
 ]
 let Numero = Math.ceil(2 * Math.random())
 
+if (typeof in global.leco == 'undefined'){
+global.leco = 0
+}
+Minumero += 1
+let Minumero = global.leco
+if (Minumero == 1) return m.reply(' ya tienes una pregunta ')
 
 
 m.reply(`
 ${global.yeca[Numero]}   
 Respuesta : ${global.logi[Numero]}
-${Numero}                       
+${Numero}     
 `.trim()) 
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
   } else return
