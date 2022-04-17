8b0a7e835e8a9df37c776df6f77626460d1c4995
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-17 11:52:46 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -2,9 +2,13 @@ let handler = async (m, {conn}) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastme > 4400) {
 global.yeca = [ 'hola','numero'
 ]
+global.logi = [ 'funda','cepillo'
+let Numero = Math.random(0, 1)
+
 
 m.reply(`
-${global.yeca[0]}                             
+${global.yeca[0]}   
+Respuesta : ${global.logi[0]}                         
 `.trim()) 
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
   } else return
