d37302e17a407235dbf1f426c81e9a4c41c29fea
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 06:57:54 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -38,7 +38,8 @@ _${usedPrefix}respuesta_ _ejemplo_
 } else {
   if (new Date - global.DATABASE._data.users[m.sender].lastme > 4400) {
 
-let Numero = Math.ceil(2 * Math.random())
+
+let Numero = Math.ceil(4 * Math.random())
 //if (!args || !args[0]) {
 //conn.reply(m.chat, `No es así!\n\n*tenes que po `, m)
 //return
