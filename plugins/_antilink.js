7be791177a04dd660a7820f6c1b41b5892995918
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 09:33:01 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -3,7 +3,10 @@ let handler = m => m
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = function (m, { user }) {
 let veces = global.lll
-if (m.text === global.logi[veces]) { return conn.reply(m.chat, `Ganaste +100 exp`, m) }
+if (m.text === global.logi[veces]) { 
+global.lll -= veces
+global.DATABASE._data.users[m.sender].exp += 100
+ conn.reply(m.chat, `Ganaste +100 exp`, m) }
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isLink = linkRegex.exec(m.text)
