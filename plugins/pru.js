81bd3679fcd9ff1fef7d4fda585b86cc6de51c7c
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 02:07:56 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -2,22 +2,27 @@
 
 let handler = async (m, {conn, args}) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastme > 4400) {
-global.yeca = [ 'nada','numero','castilll'
+global.yeca = [ 'hola','numero','castilll'
 ]
-global.logi = [ 'nada','cepillo','coron'
+global.logi = [ 'funda','cepillo','coron'
 ]
 let Numero = Math.ceil(2 * Math.random())
 if (!args || !args[0]) return conn.reply(m.chat, `No es así!\n\n*tenes que po`, m)
 let text = args.join` `
 if (args[0] === global.logi[Numero]) return conn.reply(m.chat, `Ganaste `, m)
 
-if (Numero > 1) return m.reply(`sigue en juego`)
+if(typeof global.DATABASE.data.users[user] == 'undefined') {
+  global.lupi = {
+			loto: 0
+		}
+
 
 m.reply(`
 ${global.yeca[Numero]}   
 Respuesta : ${global.logi[Numero]}
 ${Numero}    
-
+${text}
+${global.lupi.loto}
 
 `.trim()) 
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
