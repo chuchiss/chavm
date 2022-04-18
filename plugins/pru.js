b0856bad66fea1f4de43817795f735c0843676b6
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-17 23:55:41 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -1,17 +1,21 @@
-let handler = async (m, {conn}) => {
+global.yuni = global.yuni ? global.yuni : {}
+
+let handler = async (m, {conn, text}) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastme > 4400) {
 global.yeca = [ 'hola','numero','castilll'
 ]
 global.logi = [ 'funda','cepillo','coron'
 ]
 let Numero = Math.ceil(2 * Math.random())
-
+let res = global.yogi[Numero]
+if (text == res) return m.reply('bien')
 
 
 m.reply(`
 ${global.yeca[Numero]}   
 Respuesta : ${global.logi[Numero]}
 ${Numero}     
+
 `.trim()) 
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
   } else return
