bec73a88fd574ab140b8e8908314b327db9e4d11
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 03:40:56 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -7,13 +7,17 @@ global.yeca = [ 'hola','numero','castilll'
 global.logi = [ 'funda','cepillo','coron'
 ]
 let Numero = Math.ceil(2 * Math.random())
-if (!args || !args[0]) return conn.reply(m.chat, `No es así!\n\n*tenes que po ${Numero}`, m)
+if (!args || !args[0]) {
+delete global.lll[id]
+conn.reply(m.chat, `No es así!\n\n*tenes que po ${Numero}`, m)
+}
 let text = args.join` `
 if (args[0] === global.logi[Numero]) return conn.reply(m.chat, `Ganaste `, m)
+let id = m.chat
+global.lll[id] =[ '1']
 
-global.lll =[ '0']
 
-global.lll += 1
+
 m.reply(`
 ${global.yeca[Numero]}   
 Respuesta : ${global.logi[Numero]}
