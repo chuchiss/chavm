b6da0eb20fed04a5685fd0e946c878c86d5e9fe7
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 04:26:05 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -15,7 +15,10 @@ return
 let text = args.join` `
 if (args[0] === global.logi[Numero]) return conn.reply(m.chat, `Ganaste `, m)
 let id = m.chat
-
+if (typeof global.lll == 'undefined){
+ global.lll = {
+  cl: 0
+}
 
 m.reply(`
 ${global.yeca[Numero]}   
