2fd746a7c294204be85701a93c4a978ca60655db
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 06:27:58 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -11,7 +11,7 @@ global.logi = [
 ]
 let handler = async (m, {conn, args}) => {
 let veces = global.lll
-if (veces > 1) {
+if (global.lll < 6) {
 if (args[0] === global.logi[veces]) { conn.reply(m.chat, `Ganaste `, m)
 global.lll -= veces
 return
