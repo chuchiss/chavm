101550e21e173b781f2c4d47c887f708b7684d86
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-18 12:32:56 -0300

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -9,8 +9,9 @@ let handler  = async (m, { conn, args }) => {
   let { url, width, height } = pickRandom(results) || {}
   if (!url) return m.reply('sticker no encontrado')
    let linkRegex = /([0-9A-Za-z]{20,24})\/.webp/i
-  if (isLink) m.reply(`hola`)
-let isLink = linkRegex.exec(url)
+  let isLink = linkRegex.exec(url)
+if (isLink) m.reply(`hola`)
+
   conn.sendFile(m.chat, url, 'gimage', `
 sticker random
 `.trim(), m)
