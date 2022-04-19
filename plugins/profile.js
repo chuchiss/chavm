cc179d6188790b894b75e1ab48dd59024b4df923
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-19 13:51:59 -0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -7,21 +7,7 @@ let handler = async (m, { conn }) => {
   try {
     pp = await conn.getProfilePicture(who)
   } catch (e) {
-          let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, limit, exp, lastclaim, registered, warning, robos, like, banned, regTime, age, level } = global.DATABASE.data.users[who]
-    
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let username = conn.getName(who)
-    let str = `
-Nombre: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nEstado: ' + about : ''}
-${exp} Exp
-Limit: ${limit}
-Mg: ${like}
-Advertencias: ${warning} / 5
-Robos: ${robos}
-Ban: ${banned ? 'si' : 'no'}
-`.trim()
-    let mentionedJid = [who]
+
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
 
   } finally {
