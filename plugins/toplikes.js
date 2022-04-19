5cfef8cbee4c0800243bbdeefc872b7ddca3cfa6
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-19 14:45:23 -0300

    Update toplikes.js

--- plugins/toplikes.js
+++ plugins/toplikes.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, args }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastmp > 4400) {
-  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].like - a[1].like)
+  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].preg - a[1].preg)
   let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].robos - a[1].robos)
   let usersExp = sortedExp.map(v => v[0])
   let usersLim = sortedLim.map(v => v[0])
   let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
   let text = `
-• *LIKES Ranking top ${len}* •
-${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.like + ' like*').join`\n`}
+• *PREGUNTADOS Ranking top ${len}* •
+${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.preg + ' ✔️*').join`\n`}
 
 • *Robos Ranking ${len}* •
 ${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.robos + ' Robos*').join`\n`}
