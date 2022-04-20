58a5ba2921955631bdbb0cb3b24b7a887d6440cc
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 03:35:51 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -54,6 +54,37 @@ function pickkRandom(list) {
 }
 
 global.bucina = [
-  'los mejores memes jpg'
+  'meme espanol 2019 jpg',
+  'meme graciosos de autos',
+  'meme espanol jpg',
+  'meme graciosos 2020',
+  'meme espanol 2018',
+  'meme gracioso de bicis',
+  'meme espanol 2021',
+  'meme graciosos de perros',
+  'meme espanol de gatos',
+  'meme graciosos arbol',
+  'meme enfermos gatos',
+  'meme graciosos',
+  'meme ultimo meme del dia',
+  'meme sobrenatural 2019',
+  'meme graciosos de autos',
+  'meme espanol ciudades',
+  'meme graciosos 2020',
+  'meme lindo 2021',
+  'meme de peliculas',
+  'meme boludos',
+  'meme gracioso de bicis',
+  'meme espanol 2021',
+  'meme graciosos de perros',
+  'meme instagram',
+  'meme osos arbol',
+  'meme argetina',
+  'meme colombia',
+  'meme brasil',
+  'meme afeminado',
+  'meme chile',
+  'meme venezuela',
+  'meme hd 4k'
   
 ]
