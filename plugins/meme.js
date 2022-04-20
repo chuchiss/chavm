336e94c99a6625b067c0d46601d7f97ae05210a0
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 17:29:29 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -10,27 +10,11 @@ let handler  = async (m, { conn, args }) => {
   if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
   
 let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
-  if ((churro) !== 'jpg') {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return
-}
+
   conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
 
-
-catch (e) {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return
-}
 }
 handler.help = ['meme', 'memes']
 handler.tags = ['internet', 'tools']
