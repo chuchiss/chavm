86fbbc1730d79132864d1297501fa9f70dc2465d
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 05:42:39 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -15,13 +15,7 @@ let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
   if ((churro) !== 'jpg') {
 let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return}
-  conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
+}
 
  global.DATABASE._data.users[m.sender].lastmp = new Date * 1
  } else return
@@ -45,20 +39,7 @@ MEME
 `.trim(), m)
 } finally{
 
-let text = `“${pickkRandom(global.bucina)}”`
-  let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (!gis)  console.error(e)
-let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
-  if ((churro) !== 'jpg') {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
 conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return}
-  conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
 
