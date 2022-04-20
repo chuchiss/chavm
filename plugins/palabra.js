48834d7a2b8e0c76955737419bde184b9df7f0ba
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 07:45:16 -0300

    Create palabra.js

---
+++ plugins/palabra.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, user }) => {
+if (new Date - global.DATABASE._data.users[m.sender].lastmp > 9500) {
+function gero() {
+
+ conn.reply(m.chat, `*∆perdiste*  Intentalo de nuevo!`, m)
+} 
+setTimeout(gero, 9500)
+
+  conn.reply(m.chat, `*∆ di la palabra`, m)
+
+global.DATABASE._data.users[m.sender].lastmp = new Date * 1
+  } else return
+}
+handler.help = ['palabra']
+handler.tags = ['game']
+handler.limits = true
+handler.group = true
+handler.command = /^(pal(abra)?)$/i
+module.exports = handler
