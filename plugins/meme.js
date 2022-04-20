d80fd884a0ec996b0c7d3974612af60e1bef0df0
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 06:16:17 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -4,7 +4,6 @@ let gis = promisify(_gis)
 let Rimage = require('../lib/image')
 
 let handler  = async (m, { conn, args }) => {
-try{
   if (new Date - global.DATABASE._data.users[m.sender].lastmp > 2400) {
   let text = `“${pickkRandom(global.bucina)}”`
   let results = await gis(text) || []
@@ -19,34 +18,11 @@ conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
 }
-
- global.DATABASE._data.users[m.sender].lastmp = new Date * 1
- } else return
-} catch (e){
-
-let text = `“${pickkRandom(global.bucina)}”`
-  let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (!gis)  console.error(e)
-let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
-  if ((churro) !== 'jpg') {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-}
   conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
-} finally{
 
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-
-}
+}else return
 }
 handler.help = ['meme', 'memes']
 handler.tags = ['internet', 'tools']
