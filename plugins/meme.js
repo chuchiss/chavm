ec2a174193ef9b2f9d4ee527b1137b02dac91e16
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 05:18:57 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -9,24 +9,11 @@ let handler  = async (m, { conn, args }) => {
   let text = `“${pickkRandom(global.bucina)}”`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (!gis)  console.error(e)
-let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
-  if ((churro) !== 'jpg') {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return}
-  conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
+
 return
  global.DATABASE._data.users[m.sender].lastmp = new Date * 1
  } else return
 } catch(e){
-let handler  = async (m, { conn, args }) => {
 let text = `“${pickkRandom(global.bucina)}”`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
@@ -45,24 +32,10 @@ MEME
 `.trim(), m)
 } finally{
 
-let text = `“${pickkRandom(global.bucina)}”`
-  let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-  if (!url) return conn.reply(m.chat, `imagen no encontrado`, m)
-  if (!gis)  console.error(e)
-let churro = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
-  if ((churro) !== 'jpg') {
-let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-conn.sendFile(m.chat, url, 'gimage', `
-MEME
-`.trim(), m)
-return}
+
   conn.sendFile(m.chat, url, 'gimage', `
 MEME
 `.trim(), m)
-return
-}
 }
 handler.help = ['meme', 'memes']
 handler.tags = ['internet', 'tools']
