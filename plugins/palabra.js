a6fc22c2bb0d7e44d8f4cf1fc30bfbf450626915
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-20 08:32:59 -0300

    Update palabra.js

--- plugins/palabra.js
+++ plugins/palabra.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, user }) => {
 if (new Date - global.DATABASE._data.users[m.sender].lastmp > 9500) {
 function gero() {
-let lui = 3
+let lui = 13
 
 let Numero = Math.ceil(lui * Math.random())
  conn.reply(m.chat, `*${global.reee[Numero]}* de nuevo!`, m)
 global.reeu += Numero
 } 
-setTimeout(gero, 1300)
+setTimeout(gero, 5300)
 
-  conn.reply(m.chat, `*∆ di la palabra ${global.reeu}`, m)
+  conn.reply(m.chat, `*di la palabra a continuación*\n_Empieza en 5 segundos_ ..`, m)
 
 global.DATABASE._data.users[m.sender].lastmp = new Date * 1
   } else return
@@ -22,6 +22,6 @@ handler.command = /^(pal(abra)?)$/i
 module.exports = handler
 handler.owner = true
 
-global.reee = [ 'caca','shampoo','pergamino' ]
+global.reee = [ 'himNo','shampoo','pergamino','sArten', 'Equilatero','trapiTO','poLIcia','Cuartel','CaTeter','TRAMPOLIN','carTeRa','kilo','gorra','refriGeraDor']
 
 global.reeu = 0
