4d8df6c70ea1f4b3b5627d7af3d81e6d78801d71
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-21 13:56:52 -0300

    Create prueba2.js

---
+++ plugins/prueba2.js
@@ -0,0 +1,22 @@
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let readmore = readMore
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+m.reply(`
+
+
+
+
+${readmore}
+√
+`)
+
+}
+
+handler.help = ['']
+handler.tags = ['']
+handler.command = /^prueba2$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.private = false
