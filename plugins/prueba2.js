e0983dcbe39ef4b3700dabbbe5655b6a0f431d28
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-21 16:05:23 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -8,8 +8,57 @@ m.reply(`
 
 
 
-${readmore}
-√
+╭╭─「⚫🄿🅁🄸🄽🄲🄸🄿🄰🄻」
+│ Tienes *%limit Limit* *%exp XP* 
+│.menu
+│.perfil
+│.ranking
+│.top2
+│.comandos
+│.buy (comprar exp)
+│.buyall (comprar toda la exp)
+│.reclamar
+│.pagar @mension <cantidad de │exp>
+│.robar @mension <cantidad de │exp>
+│.link
+│.ping
+│.politica
+│.creditos
+╰────
+%readmore
+╭─「🔹🄰🄲🅃🄸🅅🄸🄳🄰🄳🄴🅂  
+│.attp Texto
+│.ttp Texto 
+│.style texto
+│.sticker (seleccionando imagen)
+│.imagen texto
+│.frases
+│.memes
+│.play (nombre de la musica)
+│.play2 (nombre del video)
+│.random (canción random)
+│.stickerly
+│.estados
+│.anime
+│.google (busqueda de wikipedia)
+│.traducir idioma ( marca el texto ) 
+│.tts texto
+│.s
+│.simi pregunta
+│.twittear (quue esta pasando?)
+│.twit (ver twits)
+╰────
+%readmore
+╭─「➿🄹🅄🄴🄶🄾🅂
+│.mate medium (ganar exp)
+│.adiv start (ganar exp)
+│.tateti
+│.tutti
+│.luck
+│.pozo
+│.palabra
+│.preguntados
+╰────
 `.trim()) 
 
 }
