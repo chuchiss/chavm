e269ca89145b7557b850c6fe10f45cdffeecde52
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-22 14:31:23 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastmp > 4400) {
-  let sortedExp = Object.entries(global.DATABASE.data.chat).sort((a, b) => b[1].preg - a[1].preg)
+  let sortedExp = Object.entries(global.DATABASE.data.chat).sort((a, b) => b[1].exp - a[1].exp)
   let sortedLim = Object.entries(global.DATABASE.data.chat).sort((a, b) => b[1].robos - a[1].robos)
   let usersExp = sortedExp.map(v => v[0])
   let usersLim = sortedLim.map(v => v[0])
