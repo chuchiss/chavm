4c118510603a7b3b57381353966ba5eaba426118
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-25 11:24:34 -0300

    Update pru.js

--- plugins/pru.js
+++ plugins/pru.js
@@ -414,7 +414,7 @@ global.ruizdi += 1
 *pregunta:* ¿ ${global.yeca[veces]} ?
 
 tiempo: 5 minutos
-Jugadores detectados: ${global.ruizdi}
+Jugadores en linea: ${global.ruizdi}
 
 Todo minusculas y sin acento
 las respuestas tienen que ser exactas
