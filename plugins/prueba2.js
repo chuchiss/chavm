f69284d9d039d6f7dd202dac03a11416aaa58ea0
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-25 18:12:27 -0300

    Create prueba2.js

---
+++ plugins/prueba2.js
@@ -0,0 +1,86 @@
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let readmore = readMore
+
+let handler  = async (m, { conn }) => {
+if (new Date - global.DATABASE._data.users[m.sender].lastme > 5400) {
+let { name, limit, exp, lastclaim, preg, registered, warning, robos, like, banned, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    
+conn.reply(m.chat, ` $global.chkn
+`, m)
+global.DATABASE._data.users[m.sender].lastme = new Date * 1
+}else return
+}
+handler.help = ['']
+handler.tags = ['']
+handler.command = /^(prueba2)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.chkn = [ '╭╭─「⚫🄿🅁🄸🄽🄲🄸🄿🄰🄻」
+│ Tienes *${limit} Limit* *${exp} XP* 
+│.menu
+│.perfil
+│.ranking
+│.top2
+│.comandos
+│.buy (comprar exp)
+│.buyall (comprar toda la exp)
+│.reclamar
+│.pagar @mension <cantidad de │exp>
+│.robar @mension <cantidad de │exp>
+│.link
+│.ping
+│.politica
+│.creditos
+╰────
+${readmore}
+╭─「🔹🄰🄲🅃🄸🅅🄸🄳🄰🄳🄴🅂  
+│.attp Texto
+│.ttp Texto 
+│.style texto
+│.sticker (seleccionando imagen)
+│.imagen texto
+│.frases
+│.memes
+│.chatear (anonymous)
+│.play (nombre de la musica)
+│.play2 (nombre del video)
+│.random (canción random)
+│.stickerly
+│.estados
+│.anime
+│.google (busqueda de wikipedia)
+│.traducir idioma ( marca el texto ) 
+│.tts texto
+│.s
+│.simi pregunta
+│.twittear (quue esta pasando?)
+│.twit (ver twits)
+╰────
+${readmore}
+╭─「➿🄹🅄🄴🄶🄾🅂
+│.mate medium (ganar exp)
+│.adiv start (ganar exp)
+│.tateti
+│.tutti
+│.luck
+│.pozo
+│.palabra
+│.preguntados
+╰────'
+]
