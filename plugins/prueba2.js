727077e78ba7dbde173bf2db6fd2f70523f55a4d
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-25 13:39:46 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -1,7 +1,8 @@
 global.prueba = ['']
 global.lupinn = 0
 let handler = async (m, { conn, text }) => {
-if (global.lupinn > 5) delete global.prueba
+if (global.lupinn > 5) { let index = global.prueba.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
+    global.prueba.splice(index, 1)}
 global.lupinn += 1
 global.prueba.push(`[${global.lupinn}] | ${text}\n`)
 conn.reply(m.chat, `${global.prueba}`, m)
