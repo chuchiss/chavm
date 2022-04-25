b8d7893cc2bdd551471c3930c2d9b5c0f2e49d24
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-04-25 13:31:30 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -3,8 +3,8 @@ global.lupinn = 0
 let handler = async (m, { conn, text }) => {
 
 global.lupinn += 1
-global.prueba.push(`${global.lupinn} | ${text}\n`)
-conn.reply(m.chat, `${global.prueba}`, m)
+global.prueba.push(`[*${global.lupinn}*] | ${text}\n`)
+conn.reply(m.chat, `_${global.prueba}_`, m)
 }
 handler.help = ['']
 handler.tags = ['']
