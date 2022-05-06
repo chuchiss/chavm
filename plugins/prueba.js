7ab91f0394a519f404e563bdd5e29b875d235550
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-06 14:04:11 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,122 +1,52 @@
-let fs = require ('fs')
-let path = require('path')
-let os = require('os')
-let { performance } = require('perf_hooks')
-let handler  = async (m, { conn, usedPrefix: _p }) => {
+let { Presence } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
 
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || `
-╭╭─「⚫🄿🅁🄸🄽🄲🄸🄿🄰🄻」
-│ Tienes *%limit Limit* *%exp XP* 
-│.menu
-│.perfil
-│.ranking
-│.top2
-│.comandos
-│.buy (comprar exp)
-│.buyall (comprar toda la exp)
-│.reclamar
-│.pagar @mension <cantidad de │exp>
-│.robar @mension <cantidad de │exp>
-│.link
-│.ping
-│.politica
-│.creditos
-╰────
-%readmore
-╭─「🔹🄰🄲🅃🄸🅅🄸🄳🄰🄳🄴🅂  
-│.attp Texto
-│.ttp Texto 
-│.style texto
-│.sticker (seleccionando imagen)
-│.imagen texto
-│.frases
-│.memes
-│.play (nombre de la musica)
-│.play2 (nombre del video)
-│.random (canción random)
-│.stickerly
-│.estados
-│.anime
-│.google (busqueda de wikipedia)
-│.traducir idioma ( marca el texto ) 
-│.tts texto
-│.s
-│.simi pregunta
-│.twittear (quue esta pasando?)
-│.twit (ver twits)
-╰────
-%readmore
-╭─「➿🄹🅄🄴🄶🄾🅂
-│.mate_ medium (ganar exp)
-│.adiv_ start (ganar exp)
-│.tateti
-│.tutti
-│.luck
-│.pozo
-│.palabra
-│.preguntados
-╰────
-%readmore
-Para ver todos los comandos.
-%readmore`
-    let header = conn.menu.header || '╭─「 %category 」 '
-    let body   = conn.menu.body   || '│ %cmd%islimit'
-    let footer = conn.menu.footer || '╰────\n'
-    let after  = conn.menu.after  || `
-     *Copyright © InsaneService* `
-    let _text  = before + '\n'
-    for (let tag in groups) {
-      _text += header.replace(/%category/g, tags[tag]) + '\n'
-      for (let menu of groups[tag]) {
-        for (let help of menu.help)
-          _text += body.replace(/%cmd/g, menu.prefix ? help : '%p' + help).replace(/%islimit/g, menu.limit ? ' (Limit)' : '')  + '\n'
-      }
-      _text += footer + '\n'
-    }
-    _text += after
-    text =  typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-    let replace = {
-      '%': '%',
-      p: _p, uptime, muptime,
-      npmname: package.name,
-      npmdesc: package.description,
-      version: package.version,
-      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
-      exp, limit, name, weton, week, date, time, totalreg,
-      readmore: readMore
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
-    conn.sendFile(m.chat,  '', text.trim(), m)
-  } catch (e) {
-    conn.reply(m.chat, 'Lo sentimos, el menú tiene un error', m)
-    throw e
-  }
-global.DATABASE._data.users[m.sender].lastme = new Date * 1
-  } else return
+let handler  = async (m, { conn, args, usedPrefix, command, isLimit, isPrems, isOwner}) => {
+
+	if (!args || !args[0]) return conn.reply(m.chat, `No es así!\n\n*tenes que poner* : _${usedPrefix + command} Hola_`, m)
+        
+	let text = args.join` `
+	fetch("https://api-sv2.simsimi.net/v2/?text=" + encodeURIComponent(text) + "&lc=es")
+  .then(res => res.json())
+  .then(batch => {
+    conn.updatePresence(m.chat, Presence.composing)
+  conn.reply(m.chat, `${batch.success}`, m)
+  }) .catch(() => { conn.reply(m.chat, `_Perdón :(_`, m) })
+if (!isPrems && !isOwner) global.DATABASE._data.users[m.sender].limit -= 1
+lang = 'es'
+res = await tts(batch.success, lang)
+  conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
 }
 handler.help = ['']
 handler.tags = ['']
-handler.command = /^(prueba)$/i
+handler.command = /^prueba$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
+handler.group = true
 handler.private = false
-
 handler.admin = false
 handler.botAdmin = false
-
 handler.fail = null
-handler.exp = 3
-
+handler.limit = false
 module.exports = handler
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+function tts(text, lang = 'id') {
+  console.log(lang, text)
+  return new Promise((resolve, reject) => {
+    try {
+      let tts = gtts(lang)
+      let filePath = path.join(__dirname, '../tmp', (1 * new Date) + '.wav')
+      tts.save(filePath, text, () => {
+          resolve(fs.readFileSync(filePath))
+          fs.unlinkSync(filePath)
+      })
+    } catch (e) { reject(e) }
+  })
 }
+
+
+
+
+
+
