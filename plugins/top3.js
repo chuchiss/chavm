0bcd5f7147971b4b7769e05cd7d36e2525c98105
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-12 03:52:18 -0300

    Update top3.js

--- plugins/top3.js
+++ plugins/top3.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
         totalM++
     }, 1000)
     let sorted = Object.entries(mCount).sort((a, b) => b[1] - a[1])
-    let pesan = sorted.map(v => `${v[0].replace(/(\d+)@.+/, '@$1')}: ${v[1]} mensajes`).join('\n')
+    let pesan = sorted.map(v => `${v[0].replace(/(\d+)@.+/, '@$1')}: ${v[1]} msg`).join('\n')
     m.reply(`• *Ranking top 3* • \n\n${pesan}`, false, { contextInfo: { mentionedJid: sorted.map(v => v[0]) } })
 }
 handler.help = ['top3']
