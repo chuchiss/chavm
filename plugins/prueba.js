f1dfb6445ee1e584532a07f3c657bb99fed77f40
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-11 22:27:28 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, participants }) => {
 	var sider = []
 	for(let i = 0; i < sum; i++) {
 		let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
-		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat > 0) && !users.isAdmin && !users.isSuperAdmin) { 
+		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat > 0)) { 
 			if (typeof global.DATABASE.data.users[member[i]] !== 'undefined'){
 				if(global.DATABASE.data.users[member[i]].whitelist == false){
 					total++
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text, participants }) => {
 			}
 		}
 	}
-	if(total == 0) return conn.reply(m.chat, `*Este grupo no tiene fantasmas:D.*`, m) 
+	//if(total == 0) return conn.reply(m.chat, `*Este grupo no tiene fantasmas:D.*`, m) 
 	// conn.reply(m.chat, `*[ SIDER CHECK ]*\n\n*Grup ${conn.getName(m.chat)}, memiliki anggota ${sum} orang dan terdapat sider (penyimak profesional) sebanyak ${total} orang.*\n\n*NB* : *“Akurasi dari fitur ini akan mencapai 85% apabila BOT sudah berada didalam grup minimal 7hr dan fitur ini tidak menghitung admin sider.”*${%readmore}\n\n${sider.map(v => '  ○ @' + v.replace(/@.+/, '')).join('\n')}`, m,{ contextInfo: { mentionedJid: sider } })
 	conn.reply(m.chat, `*[ LISTA DE ]*\n\n*Grupo ${conn.getName(m.chat)}, miembros: ${sum} participantes (Activos)*\n*${total} Fantasmitas👻.*\n\n*NB* : *“Manténgase activo en el grupo porque habrá limpieza de miembros todo el tiempo.”*\n\n*[ LIST SIDER ]*\n${sider.map(v => '  ○ @' + v.replace(/@.+/, '')).join('\n')}`, m,{ contextInfo: { mentionedJid: sider } })
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
