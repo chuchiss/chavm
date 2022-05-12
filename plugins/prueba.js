7037c8d0ea4f634045b9896292263eb15c51d4fe
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-12 04:14:29 -0300

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -3,9 +3,9 @@ let poin = 500
 let poin_lose = -100
 let handler = async (m, { conn, usedPrefix }) => {
   conn.suit = conn.suit ? conn.suit : {}
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Selesaikan suit mu yang sebelumnya'
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) return m.reply(`elesaikan suit mu yang sebelumnya`)
   if (!m.mentionedJid[0]) return m.reply(`_Siapa yang ingin kamu tantang?_\nTag orangnya.. Contoh\n\n${usedPrefix}suit @${owner[1]}`, m.chat, { contextInfo: { mentionedJid: [owner[1] + '@s.whatsapp.net'] } })
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `Orang yang kamu tantang sedang bermain suit bersama orang lain :(`
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) return m.reply (`rang yang kamu tantang sedang bermain suit bersama orang lain `)
   let id = 'suit_' + new Date() * 1
   let caption = `
 _*SUIT PvP*_
