bf29c18fd78dc7be1ba702f17701d69fe62d03a3
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-12 01:22:26 -0300

    Update prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args, participants }) => {
  if (new Date - global.DATABASE._data.users[m.sender].lastmp > 4400) {
 let member = participants.map(v => v[0])
 let i = 0
-let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
+let users = m.isGroup ? participants.find((v => v[0]) == member[i]) : {}
   let sortedExp = Object.entries(global.DATABASE.data.users[member[i]]).sort((a, b) => b[1].exp - a[1].exp)
   let sortedLim = Object.entries(global.DATABASE.data.users[member[i]]).sort((a, b) => b[1].limit - a[1].limit)
   let usersExp = sortedExp.map(v => v[0])
