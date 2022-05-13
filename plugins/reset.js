3e3b5f9b3b0a7c72192b1eec5ed77a2501a0db92
chuchiss <87209644+chuchiss@users.noreply.github.com>
2022-05-13 03:22:00 -0300

    Update reset.js

--- plugins/reset.js
+++ plugins/reset.js
@@ -2,13 +2,13 @@ let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.04
 let handler = async (m, { conn }) => {
 
-  let users = global.DATABASE._data.users
+  let users = global.DATABASE.data.users
 
  if (users[m.sender].limit > 200) {
 users[m.sender].limit -= 200
   m.reply (` Reseteado con éxito`)
-} else m.reply(`Tienes que tener más de 200 limit para hacer reset`)
-} }
+} else return m.reply(`Tienes que tener más de 200 limit para hacer reset`)
+} 
 handler.help = ['reset <resetea tu status>']
 handler.tags = ['xp']
 handler.command = /^reset$/i
